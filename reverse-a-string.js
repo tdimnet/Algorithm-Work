fa70a8b84c3db785976d33348cbc97576126af61
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-01-30 22:14:51 +0100

    Create and solve the first algorithm exercise

---
+++ reverse-a-string.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+  // Split the string into an array then reverse it and join it into a string
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
